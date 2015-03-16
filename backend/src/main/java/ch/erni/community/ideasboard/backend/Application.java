4524107cc5d3df2ad02423432063464df8e61b3e
rapasoft@gmail.com <flazolet65>
2015-03-16 13:29:34 +0100

    Created create and list endpoints

--- backend/src/main/java/ch/erni/community/ideasboard/backend/Application.java
+++ backend/src/main/java/ch/erni/community/ideasboard/backend/Application.java
@@ -2,6 +2,7 @@ package ch.erni.community.ideasboard.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Default application runner. It will run Spring Boot application inside embedded Tomcat.
--- backend/src/main/java/ch/erni/community/ideasboard/backend/model/Idea.java
+++ backend/src/main/java/ch/erni/community/ideasboard/backend/model/Idea.java
@@ -1,7 +1,6 @@
 package ch.erni.community.ideasboard.backend.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -14,6 +13,9 @@ import javax.validation.constraints.Min;
 @Document
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Idea {
 
 	@Id
